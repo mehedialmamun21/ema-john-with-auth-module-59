50bb7821747020f2e1786f8852dddaf1bfdaa640
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-18 17:05:20 +0600

    login functionality using react firebase hooks added

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -7,7 +7,7 @@
  * 6.attach form field handler and form submit handler
  * 7.npm install --save react-firebase-hooks
  * 8.useCreateUserWithEmailAndPassword from react-firebase-hooks
- * 
+ * 9.if user is created, redirect to the expected page
  */
 
 
