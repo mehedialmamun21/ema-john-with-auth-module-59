4b49bf041932e955597f27f6df3497795419c55a
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-18 16:35:45 +0600

    sign up hooks working done

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -6,6 +6,8 @@
  * 5.create Login, Signup component, setup route
  * 6.attach form field handler and form submit handler
  * 7.npm install --save react-firebase-hooks
+ * 8.useCreateUserWithEmailAndPassword from react-firebase-hooks
+ * 
  */
 
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './SignUp.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [ createUserWithEmailAndPassword,user] = useCreateUserWithEmailAndPassword(auth)
 
     const handleEmailBlur = event => {
         setEmail(event.target.value);
@@ -18,13 +23,22 @@ const SignUp = () => {
         setConfirmPassword(event.target.value);
     }
 
+    if(user){
+        navigate('/shop');
+    }
+
     const handleCreateUser = event => {
         event.preventDefault();
         setError('')
         if(password !== confirmPassword){
-            setError("Your two passwords didn't match")
+            setError("Your two passwords didn't match");
             return;
         }
+        if(password.length < 6){
+            setError("password must be 6 characters or longer");
+            return;
+        }
+        createUserWithEmailAndPassword(email,password);
     }
 
     return (
